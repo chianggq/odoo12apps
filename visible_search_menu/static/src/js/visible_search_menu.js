3ad9fa618803d6f4a5fd3c980ed54159675b5551
chianggq <chianggq@163.com>
2018-08-13 08:09:12 +0800

    add date

--- visible_search_menu/static/src/js/visible_search_menu.js
+++ visible_search_menu/static/src/js/visible_search_menu.js
@@ -2,6 +2,7 @@ odoo.define('visible_search_menu', function (require) {
 "use strict";
 
 var SearchView = require('web.SearchView');
+//added 2018.8.13
 SearchView.include({
     init: function () {
         var self = this;
